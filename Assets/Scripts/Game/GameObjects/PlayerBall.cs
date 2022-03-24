6fbe0364eb4c66007bd89c9527395395672ad231
hayeonnn <83764031+hayeonnn@users.noreply.github.com>
2022-03-24 14:16:08 +0900

    Scripts/GameObjects
    .cs파일 업로드

---
+++ Assets/Scripts/Game/GameObjects/PlayerBall.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerBall : MonoBehaviour
+{
+    public float jumpPower;
+    public int itemCount;
+    public GameManager manager;
+    bool isJump;
+    Rigidbody rigid;
+    public List<Vector3> points = new List<Vector3>();
+    private int currentLocation = 0;
+    bool hasKeyDown = false;
+
+    private float moveSpeed = 20.0f;    //이동속도(z축)
+    private float rotateSpeed = 300.0f;  //회전속도
+
+    private void Awake()
+    {
+        isJump = false;
+        rigid = GetComponent<Rigidbody>();
+        GameObject panel;
+        Transform panelTransform;
+        Vector3 panelPosition;
+
+        points.Add(this.gameObject.transform.position);
+
+        for(int i=1; i<=2; i++){
+            panel = GameObject.Find(i.ToString());
+            panelTransform = panel.transform;
+            panelPosition = panelTransform.position;
+            panelPosition.y = 0.5f;
+            points.Add(panelPosition);
+        }
+    }
+    private void checkInput(){
+        if(Input.GetKeyDown(KeyCode.J)) currentLocation++;
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J) && hasKeyDown == false) {
+            hasKeyDown = true;
+        }
+        if(hasKeyDown == true){
+            checkInput();
+            Debug.Log("Moving to: " + points[currentLocation].ToString());
+            transform.position = Vector3.MoveTowards(transform.position, points[currentLocation], Time.deltaTime * 3);
+        }
+
+        //오브젝트 회전(x축)
+        transform.Rotate(Vector3.right * rotateSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && !isJump)
+        {
+            isJump = true;
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+    }
+    void OnCollisionEnter(Collision collision) { 
+        if(collision.gameObject.tag == "Floor")
+            isJump = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            itemCount++;
+            GetComponent<AudioSource>().Play();
+            //other.gameObject.SetActive(false);
+            manager.GetItem(itemCount);
+            currentLocation++;
+            other.gameObject.tag = "ok";
+        }
+        else if (other.tag == "Finish")
+        {
+            if (itemCount == manager.totalItemCount)
+            {                
+                //Game Clear!
+                if (manager.stage == 1)
+                    SceneManager.LoadScene(0);
+                else
+                    SceneManager.LoadScene(manager.stage + 1);
+            }
+            else {
+                //Restart..
+                SceneManager.LoadScene(manager.stage);
+            }
+            //GameObject.FindGameObjectWithTag // find는 cpu많이잡아먹기때문에 잘 안씀
+        }
+    }
+}
