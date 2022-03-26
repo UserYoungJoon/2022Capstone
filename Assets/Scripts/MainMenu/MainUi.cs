891c966fae8393f0aadfda18bf317ef7ba7529f7
bnbbbb <94068731+bnbbbb@users.noreply.github.com>
2022-03-27 04:14:44 +0900

    UI

---
+++ Assets/Scripts/MainMenu/MainUi.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BTNType
+{
+    New,
+    Option,
+    Sound,
+    Back,
+    Mainback,
+    Quit,
+    Select,
+    Play,
+    Songback
+}
+
+public class MainUi : MonoBehaviour
+{
+
+}
--- Assets/Scripts/MainMenu/Sound.cs
+++ Assets/Scripts/MainMenu/Sound.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
 public class Sound : MonoBehaviour
 {
     public Slider sound_slider;
